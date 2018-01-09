42a13f4664025c54fe08b26fb2f0695f3a229ff4
amit0423 <34953742+amit0423@users.noreply.github.com>
2018-01-09 17:23:48 +0530

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -1,126 +1,77 @@
-const functions = require('firebase-functions');
-
-exports.searchProduct = functions.https.onRequest((request, response) => {
-
-	console.log("request.body.result.parameters:",request.body.result.parameters);
 
 
-switch (request.body.result.action) {
+const functions = require('firebase-functions'); // Cloud Functions for Firebase library
 
-        case 'input.welcome':
-
-             response.send({
-        speech: "hello welcome to  HalalBots."
-    });
-            break;
-
-        case 'input.types':
-
-  response.send({
-        speech: "it is product ,ingredient ,menu or place ?"
-    });
-
-
-
-            break;
-             case 'input.product_type':
-
-  response.send({
-        speech: "Do you have the barcode ?"
-    });
-
-
-
-            break;
-
-            case 'input.barcodehave':
-
-  response.send({
-        speech: "Please,Enter your barcode"
-    });
- break;
-
-   case 'input.barcodenumber':
-        let params = request.body.result.parameters;
-
-  response.send({
-       speech:  `${params.barcode}your barcode is matched,Please wait for a second.We analys your ingredient`
-    });
- break;
-
- case 'input.ingredient':
-        let params1 = request.body.result.parameters;
-
-  response.send({
-        speech:  `${params1.meat1}Happy to know your product is halal`
-    });
- break;
-
- case 'input.noproducttype':
-        let params2 = request.body.result.parameters;
-
-  response.send({
-        speech:  `${params2.nobarcodefound}No product found please either enter your brand name or product name`
-    });
- break;
-
-case 'input.brandname':
-        let params3 = request.body.result.parameters;
-
-  response.send({
-        speech:  `${params3.BrandName}your product is found,Please wait for a second.We analys your ingredient` 
-    });
- break;
-
- case 'input.lotsofproduct':
-       // let params4= request.body.result.parameters;
-
-  response.send({
-        speech:  `Lots of product found`
-    });
-
-  case 'input.brandname':
-        let params4= request.body.result.parameters;
-
-  response.send({
-        speech: `${params4.BrandName}your product is foundPlease wait for a second.We analys your ingredient`
-    });
- break;
- case 'input.menu':
-        
-  response.send({
-        speech: `Please enter your Ingredient`
-    });
- break;
-
- case 'input.place':
-        
-  response.send({
-        speech: `Work in progress`
-    });
- break;
-
- case 'input.nobarcode':
-        
-  response.send({
-        speech: `Either enter your BrandName or Product Name`
-    });
- break;
-
- case 'input.sure':
-        
-  response.send({
-        speech: `Your ingredient list is match,So Your Product is halal`
-    });
- break;
-
-        default:
-            response.send({
-                speech: "no action matched in webhook"
-            })
-    }
+exports.dialogflowFirebaseProduct = functions.https.onRequest((request, response) => {
+ 
+  if (request.body.result) {
+    searchProduct(request, response);
+  } else if (request.body.queryResult) {
+    barcodeSearch(request, response);
+  } else {
+    console.log('Invalid Request');
+    return response.status(400).end('Invalid Webhook Request');
+  }
 });
 
 
-
-
+function searchProduct (request, response) {
+  let action = request.body.result.action; 
+  let parameters = request.body.result.parameters; 
+  let inputContexts = request.body.result.contexts; 
+  let requestSource = (request.body.originalRequest) ? request.body.originalRequest.source : undefined;
+  const app = new DialogflowApp({request: request, response: response});
+  const actionHandlers = {
+    'input.welcome': () => {
+     
+        response.send({
+        speech: "hello welcome to  HalalBots."
+    });
+    
+    },
+	'input.types': () => {
+      
+        sendResponse('it is product ,ingredient ,menu or place ?'); 
+     
+    },
+    'input.product_type':()=>
+    {
+    	sendResponse('Do you have the barcode?');
+    };
+'input.barcodehave':()=>
+    {
+    	sendResponse('Enter your barcode,Please');
+    };
+	
+    'default': () => {
+     
+        let responseToUser = {
+          
+          speech: 'default else :This message is from Dialogflow\'s Cloud Functions for Firebase editor!', // spoken response
+          text: 'default else :This is from Dialogflow\'s Cloud Functions for Firebase editor! :-)' // displayed response
+        };
+        sendResponse(responseToUser);
+      
+    }
+  };
+  if (!actionHandlers[action]) {
+    action = 'default';
+  }
+  actionHandlers[action]();
+   
+  function sendResponse (responseToUser) {
+    if (typeof responseToUser === 'string') {
+      let responseJson = {};
+      responseJson.speech = responseToUser; 
+      responseJson.displayText = responseToUser; 
+      response.json(responseJson); 
+    } else {
+      let responseJson = {};
+      responseJson.speech = responseToUser.speech || responseToUser.displayText;
+      responseJson.displayText = responseToUser.displayText || responseToUser.speech;
+      responseJson.data = responseToUser.data;
+      responseJson.contextOut = responseToUser.outputContexts;
+      response.json(responseJson); 
+    }
+  }
+}
